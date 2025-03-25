2de2bcd6d3427f3a3d2354fe6fd266d8a9742448
duchetty <duchettydevendra@gmail.com>
2025-03-25 18:24:57 +0530

    main class madified

--- src/main/java/com/kafka/SpringBootKafkaOpnApplication.java
+++ src/main/java/com/kafka/SpringBootKafkaOpnApplication.java
@@ -8,6 +8,7 @@ public class SpringBootKafkaOpnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootKafkaOpnApplication.class, args);
+		System.out.println("application Done");
 	}
 
 }
